0ee18bc5a26f501e23b076d48d9609c8ab42a594
Gift Mbonani <giftmbonani@outlook.com>
2025-02-05 15:51:45 -0800

    http

--- 0x05-Node_JS_basic/6-http_express.js
+++ 0x05-Node_JS_basic/6-http_express.js
@@ -1,11 +1,16 @@
 const express = require('express');
 
+const PORT = 1245;
+const HOST = '127.0.0.1';
 const app = express();
 
-app.get('/', (req, res) => {
-  res.send('Hello ALX!');
+app.get('/', (req, resp) => {
+  resp.statusCode = 200;
+  resp.setHeader('Content-Type', 'text/plain');
+  resp.setHeader('X-Served-By', 'itsfoss');
+  resp.send('Hello ALX!');
 });
 
-app.listen(1245);
+app.listen(PORT, HOST, () => {});
 
 module.exports = app;
